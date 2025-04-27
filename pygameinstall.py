77d8de7580adcb70b19f2c8eaf4979576d1652f9
19662005 <claire_piron@brown.edu>
2025-04-27 13:19:40 -0400

    Initializingp1
    Claire: 27.04.2025. (about 45 minutes) I am trying to initialize the game window, so setting up the game window, initializing some potential colors and font styles to use later. I also learnt about pygame sys, which took a while. I was researching how to close a game window and after some research I went with sys as it says that that will help us not get stuck in the game later on (a major challenge from our last project)

--- pygameinstall.py
+++ pygameinstall.py
@@ -1,4 +1,22 @@
+#importing pygame
 import pygame
+import sys
 pygame.init()
-pygame.display.set_mode((640, 480))
-pygame.display.set_caption("Pygame Installation Test")
+#pygame.display.set_mode((640, 480))
+#pygame.display.set_caption("Pygame Installation Test")
+
+#setting up the game window
+WIDTH, HEIGHT = 640, 480
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Harm Reduction Adventure")
+
+#Setting up colors so we can use them later - can be changed later on
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+LIGHT_BLUE = (173, 216, 230) 
+DARK_BLUE = (0, 0, 139)
+
+#Setting up the font - can be changed later on
+font = pygame.font.SysFont(None, 48)
+small_font = pygame.font.SysFont(None, 36)
+
